25bca03420ab08a3b50cecbbcf7efbc0f1737cee
Christopher <christopher.phil.chin@gmail.com>
2022-08-07 21:54:39 +1000

    Initial Commit

---
+++ app/src/main/java/com/example/assignment4/User.kt
@@ -0,0 +1,14 @@
+package com.example.assignment4
+
+class User(firstName:String, lastName:String, phone:Int, email:String, password:String){
+
+    val firstName = emptyArray<String>()
+    val lastName = emptyArray<String>()
+    val phone = emptyArray<String>()
+    val email = emptyArray<String>()
+    val password = emptyArray<String>()
+
+
+
+
+}
